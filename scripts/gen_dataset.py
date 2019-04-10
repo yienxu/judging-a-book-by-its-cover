b8aeef7e16fa6a676e16fd751081e3f9bb73e76c
Yien Xu <yienxu@hotmail.com>
2019-04-10 15:58:23 -0500

    Split dataset into train, valid and test

--- scripts/gen_dataset.py
+++ scripts/gen_dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 
@@ -7,12 +8,25 @@ from PIL import Image
 
 CSV_PATH = 'book_data.csv'
 IMG_PATH = 'images'
-DATASET_PATH = 'all.csv'
-
+DATASET_DIR = 'dataset'
+ALL_PATH = os.path.join(DATASET_DIR, 'all.csv')
+TRAIN_PATH = os.path.join(DATASET_DIR, 'train.csv')
+VALID_PATH = os.path.join(DATASET_DIR, 'valid.csv')
+TEST_PATH = os.path.join(DATASET_DIR, 'test.csv')
 LEVELS = 10
+SPLITS = [0.8, 0.1, 0.1]
+RANDOM_STATE = 123
+
+if not os.path.isdir(DATASET_DIR):
+    os.mkdir(DATASET_DIR)
 
 
 ##### Helpers
+
+def get_filename_from_index(index):
+    img = str(index) + '.jpg'
+    return os.path.join(IMG_PATH, img)
+
 
 def get_list_of_levels(length, levels):
     chunk_size = length / levels
@@ -28,9 +42,14 @@ def get_list_of_levels(length, levels):
     return ret
 
 
-def get_filename_from_index(index):
-    img = str(index) + '.jpg'
-    return os.path.join(IMG_PATH, img)
+def split_train_valid_test(df, splits):
+    df = df.sample(frac=1, random_state=RANDOM_STATE)
+    size = df.shape[0]
+    splits = np.cumsum(SPLITS)
+    train = df[:int(size * splits[0])]
+    valid = df[int(size * splits[0]):int(size * splits[1])]
+    test = df[int(size * splits[1]):int(size * splits[2])]
+    return train, valid, test
 
 
 ##### Main
@@ -62,7 +81,22 @@ def main():
     # generate labels
     df['label'] = get_list_of_levels(df.shape[0], LEVELS)
 
-    df.to_csv(DATASET_PATH, index=None)
+    df.to_csv(ALL_PATH, index=None)
+
+    # split df into train, valid, and test
+    split_dfs = [None, None, None]
+    for lvl in range(LEVELS):
+        df_lvl = df[df['label'] == lvl]
+        ret = split_train_valid_test(df_lvl, SPLITS)
+        for i, split in enumerate(ret):
+            if split_dfs[i] is None:
+                split_dfs[i] = split
+            else:
+                split_dfs[i] = pd.concat([split_dfs[i], split])
+
+    split_dfs[0].to_csv(TRAIN_PATH, index=None)
+    split_dfs[1].to_csv(VALID_PATH, index=None)
+    split_dfs[2].to_csv(TEST_PATH, index=None)
 
 
 if __name__ == '__main__':
